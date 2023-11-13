efc4150a59a652c2a6182d20eac47c718017158e
Pierre Raybaut <p.raybaut@codra.fr>
2023-11-13 17:24:58 +0100

    Fixed NSIS installer build script

--- scripts/build_installer.bat
+++ scripts/build_installer.bat
@@ -5,17 +5,22 @@ REM ------------------------------------------
 REM Copyright (c) 2021 CODRA
 REM ======================================================
 call %~dp0utils GetScriptPath SCRIPTPATH
-call %FUNC% GetLibName LIBNAME
-call %FUNC% GetVersion VERSION
 set ROOTPATH=%SCRIPTPATH%\..\
 cd %ROOTPATH%
 
+call %FUNC% GetLibName LIBNAME
+call %FUNC% SetPythonPath
+call %FUNC% UsePython
+call %FUNC% GetVersion VERSION
+call %FUNC% GetVersionWithoutAlphaBeta VERSION_WITHOUT_ALPHABETA
+
+@REM Building NSIS installer
 set NSIS_DIST_PATH=%ROOTPATH%\dist\%LIBNAME%
 set NSIS_PRODUCT_NAME=%LIBNAME%
 set NSIS_PRODUCT_ID=%LIBNAME%
 set NSIS_INSTALLDIR=C:\%LIBNAME%
 set NSIS_PRODUCT_VERSION=%VERSION%
-set NSIS_INSTALLER_VERSION=%VERSION%.0
+set NSIS_INSTALLER_VERSION=%VERSION_WITHOUT_ALPHABETA%.0
 "C:\Program Files (x86)\NSIS\makensis.exe" nsis\installer.nsi
 
 call %FUNC% EndOfScript
